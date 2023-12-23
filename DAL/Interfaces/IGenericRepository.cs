f30f00a2f6bb5eda9232ba824e8398148d3c3bda
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-23 21:49:23 +0200

    add GenericRepository...

--- DAL/Interfaces/IGenericRepository.cs
+++ DAL/Interfaces/IGenericRepository.cs
@@ -5,6 +5,9 @@ public interface IGenericRepository<T> where T : class
     Task<T> GetByIdAsync(int id);
     Task<IList<T>> ListAllAsync();
     Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetEntityWithSpec(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    Task<int> CountAsync(ISpecification<T> spec);
     void Add(T entity);
     void Update(T entity);
     void Delete(T entity);
---
+++ DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,83 @@
+﻿using DAL.Context;
+using DAL.Interfaces;
+using DAL.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected readonly SocialNetworkContext _context;
+    protected DbSet<T> _dbSet;
+
+    public GenericRepository(SocialNetworkContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IList<T>> ListAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+    {
+        return await ApplySpecification(spec).FirstOrDefaultAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+    {
+        return await ApplySpecification(spec).ToListAsync();
+    }
+
+    public async Task<int> CountAsync(ISpecification<T> spec)
+    {
+        return await ApplySpecification(spec).CountAsync();
+    }
+
+    public void Add(T entity)
+    {
+        _dbSet.Add(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public async Task DeleteByIdAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+
+        if (entity != null)
+        {
+            _context.Entry(entity).State = EntityState.Deleted;
+        }
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+    
+    private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+    {
+        return SpecificationEvaluator<T>.GetQuery(_dbSet.AsQueryable(), spec);
+    }
+}
