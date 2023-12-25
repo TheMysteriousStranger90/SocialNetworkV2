f630dc06b28609f94058bd9b91caa67374c0514b
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-25 13:25:37 +0200

    fix AppUserDto

--- BLL/DTOs/AppUserDto.cs
+++ BLL/DTOs/AppUserDto.cs
@@ -2,11 +2,7 @@
 
 public class AppUserDto
 {
-    public int Id { get; set; }
     public string UserName { get; set; }
-    public DateOnly DateOfBirth { get; set; }
-    public DateTime Created { get; set; }
-    public DateTime LastActive { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string City { get; set; }
