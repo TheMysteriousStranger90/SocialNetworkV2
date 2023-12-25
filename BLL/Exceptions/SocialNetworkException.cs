58700172c0c879ebd1ef649bed65ab5721f81478
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-25 11:36:05 +0200

    add basic exceptions and extensions in BLL...

---
+++ BLL/Exceptions/SocialNetworkException.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace BLL.Exceptions;
+
+[Serializable]
+public class SocialNetworkException : Exception
+{
+    private static readonly string DefaultMessage = "Exception was thrown.";
+
+    public SocialNetworkException() : base(DefaultMessage)
+    {
+    }
+
+    public SocialNetworkException(string message) : base(message)
+    {
+    }
+
+    public SocialNetworkException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    protected SocialNetworkException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+}
