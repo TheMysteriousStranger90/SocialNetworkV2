f5cac4d1a2537fc3027eaeff1551c35adb392ff9
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-29 16:58:12 +0200

    add FollowController and NotificationsController (redis cache...)

---
+++ WebAPI/Controllers/FollowController.cs
@@ -0,0 +1,52 @@
+﻿using BLL.DTOs;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Authorize]
+public class FollowController : BaseApiController
+{
+    private readonly IFollowService _followService;
+
+    public FollowController(IFollowService followService)
+    {
+        _followService = followService;
+    }
+
+    [HttpGet("{userName}/followed")]
+    public async Task<ActionResult<IEnumerable<AppUserDto>>> GetFollowedUsers(string userName)
+    {
+        var followedUsers = await _followService.GetFollowedUsersAsync(userName);
+        return Ok(followedUsers);
+    }
+
+    [HttpGet("{userName}/followers")]
+    public async Task<ActionResult<IEnumerable<AppUserDto>>> GetFollowers(string userName)
+    {
+        var followers = await _followService.GetFollowersAsync(userName);
+        return Ok(followers);
+    }
+
+    [HttpPost("{userName}/follow/{followedUserName}")]
+    public async Task<ActionResult> FollowUser(string userName, string followedUserName)
+    {
+        await _followService.FollowUserAsync(userName, followedUserName);
+        return Ok();
+    }
+
+    [HttpDelete("{userName}/unfollow/{followedUserName}")]
+    public async Task<ActionResult> UnfollowUser(string userName, string followedUserName)
+    {
+        await _followService.UnfollowUserAsync(userName, followedUserName);
+        return Ok();
+    }
+
+    [HttpGet("{userName}/is-following/{otherUserName}")]
+    public async Task<ActionResult<bool>> IsFollowing(string userName, string otherUserName)
+    {
+        var isFollowing = await _followService.IsFollowingAsync(userName, otherUserName);
+        return Ok(isFollowing);
+    }
+}
---
+++ WebAPI/Controllers/NotificationsController.cs
@@ -0,0 +1,62 @@
+﻿using BLL.DTOs;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Helpers;
+
+namespace WebAPI.Controllers;
+
+[Authorize]
+public class NotificationsController : BaseApiController
+{
+    private readonly INotificationService _notificationService;
+
+    public NotificationsController(INotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    [HttpPost("{userName}")]
+    public async Task<ActionResult> SendNotification(string userName, [FromBody] string content)
+    {
+        await _notificationService.SendNotificationAsync(userName, content);
+        return Ok();
+    }
+
+    [HttpPut("{userName}/read/{notificationId}")]
+    public async Task<ActionResult> MarkAsRead(string userName, int notificationId)
+    {
+        await _notificationService.MarkAsReadAsync(userName, notificationId);
+        return Ok();
+    }
+
+    [HttpPut("{userName}/read-all")]
+    public async Task<ActionResult> MarkAsReadAll(string userName)
+    {
+        await _notificationService.MarkAsReadAllAsync(userName);
+        return Ok();
+    }
+
+    [Cached(600)]
+    [HttpGet("{userName}")]
+    public async Task<ActionResult<IEnumerable<NotificationDto>>> GetNotificationsByUserId(string userName)
+    {
+        var notifications = await _notificationService.GetNotificationsByUserIdAsync(userName);
+        return Ok(notifications);
+    }
+
+    [Cached(600)]
+    [HttpGet("{userName}/unread")]
+    public async Task<ActionResult<IEnumerable<NotificationDto>>> GetUnreadNotificationsByUserId(string userName)
+    {
+        var notifications = await _notificationService.GetUnreadNotificationsByUserIdAsync(userName);
+        return Ok(notifications);
+    }
+
+    [HttpPost("photo-notification/{albumId}")]
+    public async Task<ActionResult> CreatePhotoToAlbumNotification(int albumId, [FromBody] PhotoDto photoDto)
+    {
+        await _notificationService.CreatePhotoToAlbumNotificationAsync(albumId, photoDto);
+        return Ok();
+    }
+}
