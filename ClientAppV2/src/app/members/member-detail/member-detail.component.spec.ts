a09a52ca4045cfd7a3b28058702c729f0a942746
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-01-08 09:35:07 +0200

    ...

---
+++ ClientAppV2/src/app/members/member-detail/member-detail.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MemberDetailComponent } from './member-detail.component';
+
+describe('MemberDetailComponent', () => {
+  let component: MemberDetailComponent;
+  let fixture: ComponentFixture<MemberDetailComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MemberDetailComponent]
+    });
+    fixture = TestBed.createComponent(MemberDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
